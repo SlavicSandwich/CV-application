010036d4d3b9da7a1bcd6fa757a6844dcd1a1348
SlavicSandwich <78306203+SlavicSandwich@users.noreply.github.com>
2023-09-10 02:18:22 +0300

    add AddExperience.jsx and set up[ layout

---
+++ src/components/experience/AddExperience.jsx
@@ -0,0 +1,39 @@
+import CreateForm from "../../CreateForm";
+
+export default function AddExperience({
+  jobs,
+  isOpen,
+  onChange,
+  createForm,
+  setOpen,
+  onCancel,
+  toggleCollapsed,
+  onHide,
+  onRemove,
+}) {
+  return (
+    <div className="add-experience-section section">
+      <ExpandSection
+        isOpen={isOpen}
+        setOpen={setOpen}
+        sectionName="Experience"
+        iconName=""
+      />
+
+      <div className={`section-content ${isOpen ? "open" : ""}`}>
+        <DisplayForms
+          forms={jobs}
+          // FormComponent={JobForm}
+          onChange={onChange}
+          onCancel={onCancel}
+          onHide={onHide}
+          onRemove={onRemove}
+          toggleCollapsed={toggleCollapsed}
+          // titleKey="jobName"
+          // arrayName="jobs"
+        />
+      </div>
+      <CreateForm createForm={createForm} buttonText={Job} />
+    </div>
+  );
+}
