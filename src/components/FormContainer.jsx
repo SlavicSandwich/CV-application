6387230cadab134bebab5d804257e9605f9e7582
SlavicSandwich <78306203+SlavicSandwich@users.noreply.github.com>
2023-09-10 15:26:09 +0300

    add imports

--- src/components/FormContainer.jsx
+++ src/components/FormContainer.jsx
@@ -1,5 +1,7 @@
 import Template from "./Template";
 import AddPersonalDetails from "./personal/AddPersonalDetails";
+import AddEducation from "./education/AddEducation";
+import AddExperience from "./experience/AddExperience";
 
 export default function FormContainer() {
   return (
