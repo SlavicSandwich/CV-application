0db16b7177c30412e4cfe5522eddcbd4d2343add
SlavicSandwich <78306203+SlavicSandwich@users.noreply.github.com>
2023-09-19 23:11:44 +0300

    create info items

---
+++ src/components/education/EducationInfo.jsx
@@ -0,0 +1,19 @@
+export default function EducationInf({ info }) {
+  return (
+    <div className="education-info">
+      <div className="education-info-group">
+        <p className="dates">
+          {info.startDate}
+          {info.startDate && info.endDate && <span> - </span>}
+          {endDate}
+        </p>
+        <p>{info.location}</p>
+      </div>
+
+      <div className="education-info-group">
+        <p className="education-info-schoolName">{info.schoolName}</p>
+        <p className="eductation-info-degree">{info.degree}</p>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/experience/ExperienceInfo.jsx
@@ -0,0 +1,19 @@
+export default function ExperienceInfo({ info }) {
+  return (
+    <div className="experience-info">
+      <div className="experience-info-group">
+        <p className="dates">
+          {info.startDate}
+          {info.startDate && info.endDate && <span> - </span>}
+          {endDate}
+        </p>
+        <p>{info.location}</p>
+      </div>
+      <div className="experience-info-group">
+        <p className="experience-info-company-name">{info.companyName}</p>
+        <p className="experience-info-position-title">{info.positionTitle}</p>
+        <p className="experience-info-description">{info.description}</p>
+      </div>
+    </div>
+  );
+}
