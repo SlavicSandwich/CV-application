9e78107a24fca512a2e1878d088ce3aa808fb54e
SlavicSandwich <78306203+SlavicSandwich@users.noreply.github.com>
2023-09-19 21:54:59 +0300

    layout template for resume side

--- src/components/ResumeSide.jsx
+++ src/components/ResumeSide.jsx
@@ -1,7 +1,18 @@
-export default function ResumeSide() {
+export default function ResumeSide({ personalInfo, sections, layout }) {
   return (
-    <>
-      <div className="resume-side"></div>
-    </>
+    <div className="resume-container">
+      <div className={`resume ${layout}`}>
+        <PersonalInfoSection
+          fullName={personalInfo.fullName}
+          email={personalInfo.email}
+          phoneNumber={personalInfo.phoneNumber}
+          adress={personalInfo.adress}
+        />
+        <div>
+          <EducationInfoSection educations={sections.educations} />
+          <ExperienceInfoSection educations={sections.jobs} />
+        </div>
+      </div>
+    </div>
   );
 }
