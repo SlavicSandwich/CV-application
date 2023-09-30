5d1a426f838c6695ecaa19ec35bb789227010ce2
SlavicSandwich <78306203+SlavicSandwich@users.noreply.github.com>
2023-10-01 02:26:57 +0300

    typo

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFileLines } from "@fortawesome/free-regular-svg-icons";
 import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import "../css/sidebar.css";
 
 export default function Sidebar({ onGoToPage, page }) {
   return (
@@ -8,7 +9,7 @@ export default function Sidebar({ onGoToPage, page }) {
       <button
         className="sidebar-button"
         onClick={() => onGoToPage("content")}
-        data-secelcted={page === "content"}
+        data-selected={page === "content"}
       >
         <FontAwesomeIcon icon={faFileLines} />
         <p>Content</p>
@@ -16,7 +17,7 @@ export default function Sidebar({ onGoToPage, page }) {
       <button
         className="sidebar-button"
         onClick={() => onGoToPage("customize")}
-        data-secelcted={page === "cutsomize"}
+        data-selected={page === "customize"}
       >
         <FontAwesomeIcon icon={faPenToSquare} />
         <p>Customize</p>
