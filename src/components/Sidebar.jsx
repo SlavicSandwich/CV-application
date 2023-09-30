479acded00c3b732a8d4c7f659e2131ed91ebd7b
SlavicSandwich <78306203+SlavicSandwich@users.noreply.github.com>
2023-10-01 01:44:31 +0300

    fix sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,17 +2,25 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFileLines } from "@fortawesome/free-regular-svg-icons";
 import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
 
-export default function Sidebar() {
+export default function Sidebar({ onGoToPage, page }) {
   return (
-    <div className="sidebar">
-      <button className="sidebar-button">
+    <nav className="sidebar">
+      <button
+        className="sidebar-button"
+        onClick={() => onGoToPage("content")}
+        data-secelcted={page === "content"}
+      >
         <FontAwesomeIcon icon={faFileLines} />
         <p>Content</p>
       </button>
-      <button className="sidebar-button">
+      <button
+        className="sidebar-button"
+        onClick={() => onGoToPage("customize")}
+        data-secelcted={page === "cutsomize"}
+      >
         <FontAwesomeIcon icon={faPenToSquare} />
         <p>Customize</p>
       </button>
-    </div>
+    </nav>
   );
 }
