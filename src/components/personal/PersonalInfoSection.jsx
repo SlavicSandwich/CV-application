e20ffc789223431127ceed5249a616619c64eebf
SlavicSandwich <78306203+SlavicSandwich@users.noreply.github.com>
2023-10-08 02:15:47 +0300

    add personalInfoSection.css, fix typo

--- src/components/personal/PersonalInfoSection.jsx
+++ src/components/personal/PersonalInfoSection.jsx
@@ -1,36 +1,37 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 import { faPhone } from "@fortawesome/free-solid-svg-icons";
-
 import { faMapLocationDot } from "@fortawesome/free-solid-svg-icons";
-
+import "../../css/personalInfoSection.css";
 export default function PersonalInfoSection({
   email,
   fullName,
   phoneNumber,
   adress,
 }) {
-  <div className="personal-info">
-    <h1 className="resume-name">{fullName}</h1>
-    <div className={"contact-info"}>
-      {email && (
-        <div>
-          <FontAwesomeIcon icon={faEnvelope} />
-          <span>{email}</span>
-        </div>
-      )}
-      {phoneNumber && (
-        <div>
-          <FontAwesomeIcon icon={faPhone} />
-          <span>{phoneNumber}</span>
-        </div>
-      )}
-      {adress && (
-        <div>
-          <FontAwesomeIcon icon={faMapLocationDot} />
-          <span>{adress}</span>
-        </div>
-      )}
+  return (
+    <div className="personal-info">
+      <h1 className="resume-name">{fullName}</h1>
+      <div className={"contact-info"}>
+        {email && (
+          <div>
+            <FontAwesomeIcon icon={faEnvelope} />
+            <span>{email}</span>
+          </div>
+        )}
+        {phoneNumber && (
+          <div>
+            <FontAwesomeIcon icon={faPhone} />
+            <span>{phoneNumber}</span>
+          </div>
+        )}
+        {adress && (
+          <div>
+            <FontAwesomeIcon icon={faMapLocationDot} />
+            <span>{adress}</span>
+          </div>
+        )}
+      </div>
     </div>
-  </div>;
+  );
 }
