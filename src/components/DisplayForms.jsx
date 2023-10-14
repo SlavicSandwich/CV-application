c5b0f72ebfaf4ce0b27b6062308de0ab35af1f8f
SlavicSandwich <78306203+SlavicSandwich@users.noreply.github.com>
2023-10-14 19:34:28 +0300

    change the way forms are displayed

--- src/components/DisplayForms.jsx
+++ src/components/DisplayForms.jsx
@@ -1,4 +1,5 @@
 import CollapsedForm from "./CollapsedForm";
+import React from "react";
 
 export default function DisplayForms({
   forms,
@@ -11,7 +12,27 @@ export default function DisplayForms({
   titleKey,
   arrayName,
 }) {
-  return (
+  let flag = false;
+  let form_return;
+  forms.forEach((form) => {
+    if (!form.isCollapsed) {
+      flag = true;
+      form_return = form;
+    }
+  });
+
+  return flag ? (
+    <div className="forms-container">
+      <FormComponent
+        onChange={onChange}
+        form={form_return}
+        key={form_return.id}
+        onCancel={onCancel}
+        onRemove={onRemove}
+        onSave={toggleCollapsed}
+      />
+    </div>
+  ) : (
     <div className="forms-container">
       {forms.map((form) =>
         form.isCollapsed ? (
@@ -28,12 +49,52 @@ export default function DisplayForms({
             onChange={onChange}
             form={form}
             key={form.id}
-            cancel={onCancel}
-            save={toggleCollapsed}
-            remove={onRemove}
+            onCancel={onCancel}
+            onSave={toggleCollapsed}
+            onRemove={onRemove}
           />
         )
       )}
     </div>
   );
 }
+
+// export default function DisplayForms({
+//   forms,
+//   FormComponent,
+//   onChange,
+//   onCancel,
+//   onHide,
+//   onRemove,
+//   toggleCollapsed,
+//   titleKey,
+//   arrayName,
+// }) {
+//   return (
+//     <div className="forms-container">
+//       {forms.map((form) => {
+//         <React.Fragment key={form.id}>
+//           form.isCollapsed ? (
+//           <CollapsedForm
+//             onClick={toggleCollapsed}
+//             // key={form.id}
+//             form={form}
+//             title={form[titleKey]}
+//             arrayName={arrayName}
+//             hideForm={onHide}
+//           />
+//           ) : (
+//           <FormComponent
+//             onChange={onChange}
+//             form={form}
+//             // key={form.id}
+//             cancel={onCancel}
+//             save={toggleCollapsed}
+//             remove={onRemove}
+//           />
+//           )
+//         </React.Fragment>;
+//       })}
+//     </div>
+//   );
+// }
